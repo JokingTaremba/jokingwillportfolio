a548da6db6466e4bb442555504de733befbf95dd
JokingTaremba <jokingtaremba@gmail.com>
2025-09-30 08:31:00 +0200

    feat: finalize the configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Ye() {
-  return <h1 className="text-red-500 font-bold text-xl">Jokingj</h1>;
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-200">
+      <h1 className="text-5xl font-bold text-blue-500">Tailwind</h1>
+    </main>
+  );
 }
